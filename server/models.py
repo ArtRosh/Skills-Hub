10174bdd2238971c920442330d1a38a5ef803b3d
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-02-01 10:57:49 -0800

    Modified model.py where added unique constrains on User.name

--- server/models.py
+++ server/models.py
@@ -1,13 +1,16 @@
 # server/models.py
 
 from flask_login import UserMixin
+
 from config import db
+from config import bcrypt
+
 
 class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String, nullable=False)
     role = db.Column(db.String, nullable=False)  # "student" | "tutor" 
 
