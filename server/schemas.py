0379e00cc0fb603704f28548c97d73bde733b52b
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-02-04 11:51:40 -0800

    Modified schemas.py by changing order of respond for logged in users

--- server/schemas.py
+++ server/schemas.py
@@ -1,37 +1,55 @@
+# server/schemas.py
+
+from marshmallow import fields
 from config import ma
-from models import User, Topic, TutorService, Request
+from models import User, Topic, Request
 
 
-class UserSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = User
-        load_instance = True
-        include_relationships = True
-        exclude = ("password",)
-
-class TopicSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = Topic
-        load_instance = True
-        include_relationships = True
-
-class TutorServiceSchema(ma.SQLAlchemyAutoSchema):
-    class Meta:
-        model = TutorService
-        load_instance = True
-        include_relationships = True
-
-class RequestSchema(ma.SQLAlchemyAutoSchema):
+class RequestSchema(ma.SQLAlchemySchema):
     class Meta:
         model = Request
         load_instance = True
-        include_relationships = True
 
-user_schema = UserSchema()
-users_schema = UserSchema(many=True)
-topic_schema = TopicSchema()
-topics_schema = TopicSchema(many=True)
-tutor_service_schema = TutorServiceSchema()
-tutor_services_schema = TutorServiceSchema(many=True)
+    id = ma.auto_field()
+    status = ma.auto_field()
+    description = ma.auto_field()
+    tutor_id = ma.auto_field()
+
+
 request_schema = RequestSchema()
-requests_schema = RequestSchema(many=True) 
+requests_schema = RequestSchema(many=True)
+
+
+class TopicWithRequestsSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Topic
+        load_instance = True
+
+    id = ma.auto_field()
+    topic = ma.auto_field()
+    description = ma.auto_field()
+
+    requests = fields.Nested(RequestSchema, many=True)
+
+
+topic_with_requests_schema = TopicWithRequestsSchema()
+topics_with_requests_schema = TopicWithRequestsSchema(many=True)
+
+
+class TutorSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = User
+        load_instance = True
+
+    id = ma.auto_field()
+    name = ma.auto_field()
+    role = ma.auto_field()
+
+    topics = fields.Method("get_topics")
+
+    def get_topics(self, user_obj):
+        topics = [ts.topic for ts in user_obj.tutor_services]
+        return topics_with_requests_schema.dump(topics)
+
+
+tutor_schema = TutorSchema()
