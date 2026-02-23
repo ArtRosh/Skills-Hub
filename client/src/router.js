2379ef4f371fae02ee8fc30557dda0a93fbb15f4
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-02-23 11:24:26 -0800

    Chore: Update router to use new modal page

--- client/src/router.js
+++ client/src/router.js
@@ -1,16 +1,16 @@
 // client/src/router.js
+
 import Layout from "./features/shared/components/Layout";
 import Home from "./features/shared/pages/Home";
 import ErrorPage from "./features/shared/pages/ErrorPage";
-
 import Login from "./features/auth/pages/Login";
 import Signup from "./features/auth/pages/Signup";
 import RequireAuth from "./features/shared/components/RequireAuth";
-
 import TutorTopics from "./features/tutor/pages/TutorTopics";
 import TutorServiceRequests from "./features/tutor/pages/TutorServiceRequests";
 import StudentTopics from "./features/student/pages/StudentTopics";
 import StudentServiceRequests from "./features/student/pages/StudentServiceRequests";
+import RequestServiceModal from "./features/shared/pages/RequestServiceModal";
 
 
 const routes = [
@@ -30,7 +30,7 @@ const routes = [
       { path: "student_topics", element: <RequireAuth><StudentTopics /></RequireAuth> },
       { path: "student/topic/:topicId/service/:serviceId/requests", element: <RequireAuth><StudentServiceRequests /></RequireAuth> },
       { path: "tutor/topic/:topicId/service/:serviceId/requests", element: <RequireAuth><TutorServiceRequests /></RequireAuth> },
-      
+      { path: "topic/:topicId/service/:serviceId/request", element: <RequireAuth><RequestServiceModal /></RequireAuth> },
     ],
   },
 ];
