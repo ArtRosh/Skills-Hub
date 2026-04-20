6fb9e4363541248392aaaea68ad91bfe1c53f62c
Artem Roshchupkin <artem.rosh17@gmail.com>
2026-04-20 14:31:37 -0700

    Add optional sign above comment form in order to create request as student

--- client/src/features/shared/pages/RequestServiceModal.jsx
+++ client/src/features/shared/pages/RequestServiceModal.jsx
@@ -165,7 +165,7 @@ function RequestServiceModal() {
               <p><strong>Description:</strong> {service?.description}</p>
 
               <div className="mb-3">
-                <label className="form-label" htmlFor="description">Description / Note</label>
+                <label className="form-label" htmlFor="description">Description / Note (optional)</label>
                 <textarea
                   id="description"
                   name="description"
